80766cce8d03d2b32de9c3b313317b39e369315d
Bill-Katsakos <vasilhskats@gmail.com>
2025-03-04 23:49:47 +0200

    GreenField, CashTrack, create/delete expenses test

--- backend/server.js
+++ backend/server.js
@@ -77,10 +77,45 @@ app.get("/expenses", async (req, res) => {
 
 
 
+// __________Create an Expense________
+const createExpense = async (req, res) => {
+    try {
+    //   let userId = req.user.userId;
+    //   const { description, date, amount } = req.body;
+  
+    //   let createdExpense = await Expenses.create({
+    //     description,
+    //     date,
+    //     amount,
+    //     user: userId,
+    //   });
+      let createdExpense = await Expenses.create(req.body);
+  
+      res.status(200).send({ msg: "An expense is been added successfully 🤑", createdExpense });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+  
+app.post("/expenses/add", createExpense);
 
 
+// __________Delete expense ________
+const deleteExpense = async (req, res) => {
+    try {
 
+      const deleteExpense = await Expenses.findByIdAndDelete(req.params.id);
+      if (!deleteExpense) {
+          return res.status(404).json({ msg: "Expense not found." });
+        }
+        return res.status(200).json({ msg: "Expense successfully deleted." });
 
+    } catch (error) {
+      return res.status(500).json({ msg: "Server error", error: error.message });
+    }
+  };
+  
+  app.delete("/expenses/delete/:id", deleteExpense);
 
 
 
@@ -116,49 +151,49 @@ const registerUser = async (req, res) => {
       console.log(error);
       res.status(500).send({ msg: "Internal server error" });
     }
-  };
+};
   
-  app.post("/user/register", registerUser);
+app.post("/user/register", registerUser);
   
-  // ______________Login Route____________
-  
-  const loginUser = async (req, res) => {
+// ______________Login Route____________
+
+const loginUser = async (req, res) => {
     try {
-      const { email, password } = req.body;
-      if (!email || !password) {
+        const { email, password } = req.body;
+        if (!email || !password) {
         return res.send({ msg: "All fields are required" });
-      }
-      let oldRegisteredUser = await User.findOne({ email });
-  
-      if (!oldRegisteredUser) {
+        }
+        let oldRegisteredUser = await User.findOne({ email });
+
+        if (!oldRegisteredUser) {
         return res.send({ msg: "User not found, please register first." });
-      }
-      let isPasswordCorrect = await bcrypt.compare(
+        }
+        let isPasswordCorrect = await bcrypt.compare(
         password,
         oldRegisteredUser.password
-      );
-      if (!isPasswordCorrect) {
+        );
+        if (!isPasswordCorrect) {
         return res.send({ msg: "Wrong password." });
-      }
-      // ___________token__________
-  
-      let payload = {
+        }
+        // ___________token__________
+
+        let payload = {
         userId: oldRegisteredUser._id,
         email: oldRegisteredUser.email,
-      };
-      let token = await jwt.sign(payload, process.env.SECRET_KEY
-          // ,{
-          //  expiresIn: "1h", // expiresIn: "1h" - means that the token is expiring in 1 hour
-          // }
-      );
-  
-      return res.send({ msg: "Login Successfully", token }); 
+        };
+        let token = await jwt.sign(payload, process.env.SECRET_KEY
+            // ,{
+            //  expiresIn: "1h", // expiresIn: "1h" 
+            // }
+        );
+
+        return res.send({ msg: "Login Successfully", token }); 
     } catch (error) {
-      console.log(error);
-      res.status(500).send({ msg: "Internal server error" });
+        console.log(error);
+        res.status(500).send({ msg: "Internal server error" });
     }
-  };
-  
-  app.post("/user/login", loginUser);
+};
+
+app.post("/user/login", loginUser);
 
 app.listen(port, () => console.log(`Server is start listening on port ${port}`));
